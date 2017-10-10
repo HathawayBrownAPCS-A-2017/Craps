e7871acad4aa5c081a1e8187788fd8149675d5fe
margaretkilbane <31964814+margaretkilbane@users.noreply.github.com>
2017-10-10 15:04:00 -0400

    Add files via upload

--- CrapsGame.java
+++ CrapsGame.java
@@ -14,11 +14,44 @@ public class CrapsGame
    */
   public int processRoll(int total)
   {
+      if (point == 0)
+      {
+          if (total == 7 || total == 11)
+          {
+              point = 0;
+              return 1;
+          }
 
-    __________________________________________
-    ...
+          else if (total == 2 || total == 3 || total == 12)
+          {
+              point = 0;
+              return -1;
+          }
+          else
+          {
+              point = total;
+              return 0;
+          }
+        }
+        else
+        {
+            if (total == 7)
+            {
+                point = 0;
+                return -1;
+            }
+        
+            else if (point == total)
+            {
+                point = 0;
+                return 1;
+            }
+            else
+            {   
+                return 0;
+            }
+        }
 
-    return result;
   }
 
   /**
